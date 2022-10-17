226b810bf275f54d8813c8cbad0a31c89f447891
Macbookpro <malik.ertugrul@net-bt.com.tr>
2022-10-17 10:42:14 +0300

    Maps Screen

--- App.tsx
+++ App.tsx
@@ -1,48 +1,13 @@
-import * as React from 'react';
-import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
-import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import Maps from './src/screens/Maps'
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <MapView
-        provider={PROVIDER_GOOGLE}
-        style={styles.map}
-        region={{
-          latitude: 37.78825,
-          longitude: -122.4324,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}
-      >
-        <Marker coordinate={{
-          latitude: 37.78825,
-          longitude: -122.4324,
-        }}
-          title="This is a title"
-          description="This is a description"
-        />
-      </MapView>
-    </View>
-  );
+    <Maps />
+  )
 }
 
+export default App
 
-const styles = StyleSheet.create({
-  container: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-    justifyContent: 'flex-end',
-    alignItems: 'center',
-  },
-  map: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-  },
-});
+const styles = StyleSheet.create({})
--- src/screens/Maps.tsx
+++ src/screens/Maps.tsx
@@ -1,22 +1,48 @@
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import MapView from 'react-native-maps';
+import * as React from 'react';
+import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
 
-const Maps = () => {
-    return (
-        <MapView
-            initialRegion={{
-                latitude: 37.78825,
-                longitude: -122.4324,
-                latitudeDelta: 0.0922,
-                longitudeDelta: 0.0421,
-            }}
-        />
-    )
-    
-
+export default function Maps() {
+  return (
+    <View style={styles.container}>
+      <MapView
+        // provider={PROVIDER_GOOGLE}
+        style={styles.map}
+        region={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      >
+        {/* <Marker coordinate={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+        }}
+          title="This is a title"
+          description="This is a description"
+        /> */}
+      </MapView>
+    </View>
+  );
 }
 
-export default Maps
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  map: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+  },
+});
